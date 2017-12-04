41a97eff7b98b90b09e32b3c2b65a57f784438af
Dafne <olca.dafne@gmail.com>
2017-12-04 18:14:12 +0100

    starting up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { UpcomingComponent } from './trips/upcoming/upcoming.component';
+import { CreatedComponent } from './trips/created/created.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent,
+    UpcomingComponent,
+    CreatedComponent
   ],
   imports: [
     BrowserModule
